8ebc8f84d8ba42e0fd3a224163b39dd2f1afda57
rafakimanja <90486707+rafakimanja@users.noreply.github.com>
2024-02-29 11:16:34 -0300

    new commit

---
+++ Aulas - POO Avançado/Polimorfismo/main.py
@@ -0,0 +1,7 @@
+from modelo import Filme, Serie
+
+filme1 = Filme('Os incriveis', 2005, 90)
+serie1 = Serie('Broklyn99', 2020, 8)
+
+print(filme1)
+print(serie1)
---
+++ Aulas - POO Avançado/Polimorfismo/modelo.py
@@ -0,0 +1,43 @@
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f'Nome: {self._nome} | Ano: {self.ano} | Qtd Likes: {self._likes}'
+
+
+class Filme(Programa):
+     def __init__(self, nome, ano, duracao):
+         super().__init__(nome, ano)
+         self.duracao = duracao
+
+     def __str__(self):
+        return f'{super().__str__()} | Duração: {self.duracao}'
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f'{super().__str__()} | Duração: {self.temporadas}'
